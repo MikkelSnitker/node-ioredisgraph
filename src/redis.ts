cde31e89b8dd2e892a0a2cd713bbc1a3f08d56d5
Mikkel Snitker <mikkel@snitker.com>
2022-10-13 11:30:13 +0200

    test

--- src/redis.ts
+++ src/redis.ts
@@ -9,6 +9,17 @@ import { GraphCommand } from './GraphCommand';
 import { GraphResponse, RedisGraphResponse } from './GraphResponse';
 export { getStatistics } from './Stats'
 
+interface Sentinel {
+    sentinel(subcommand: "sentinels" | "get-master-addr-by-name" | "slaves", name: string): Promise<string[]>;
+    subscribe(...channelNames: string[]): Promise<number>;
+    on(
+        event: "message",
+        callback: (channel: string, message: string) => void
+    ): void;
+    on(event: "error", callback: (error: Error) => void): void;
+    on(event: "reconnecting", callback: () => void): void;
+    disconnect(): void;
+}
 
 declare module 'ioredis' {
 
@@ -24,76 +35,96 @@ interface AddressFromResponse {
     port: string;
     ip: string;
     flags?: string;
-  }
-  
-  
+}
+
+
 type Endpoint = { host: string, port: number };
+
+export function packObject(array: string[]): Record<string, any> {
+    const result: Record<string, string> = {};
+    const length = array.length;
+
+    for (let i = 1; i < length; i += 2) {
+        result[array[i - 1]] = array[i];
+    }
+
+    return result;
+}
+
 
 
 class Connector extends Redis.SentinelConnector {
-    private slave?: Redis.Redis;
     constructor(options: unknown) {
         super({
             ...options as any,
-            preferredSlaves(slaves:AddressFromResponse[]){
-                return slaves[Math.floor((Math.random()*slaves.length))];
-            }
         });
     }
-    
-    async getSlave(){
-        if(process.env["IOREDIS_MASTER_ONLY"]){
-            return null;
-        }
 
-
-        if(this.slave){
-            return this.slave;
-        }
-
-       let c =0 ;
-       let endpoint;
-        while(c < 2)
-        {
-            const {value, done} = this.sentinelIterator.next();    
-            if(done){
+    async getSlaves() {
+        let c = 0;
+        let endpoint;
+        while (c < 2) {
+            const { value, done } = this.sentinelIterator.next();
+            if (done) {
                 this.sentinelIterator.reset(false);
-            }  else {
+            } else {
                 endpoint = value;
                 break;
             }
-            c++;    
+            c++;
         }
-        
-        const client = (this as any).connectToSentinel(endpoint);
-        if(!client) return null;
-        endpoint = await (this as any).resolveSlave(client);
 
-        if(!endpoint) return null;
-        const { sentinels, sentinelCommandTimeout, sentinelPassword, sentinelMaxConnections, sentinelReconnectStrategy, sentinelRetryStrategy, sentinelTLS, sentinelUsername, updateSentinels, enableTLSForSentinelMode, Connector, ...options  } = ( this.options as Redis.RedisOptions & Redis.SentinelConnectionOptions)
-        const slave = new Redis.default({ ...options, ...endpoint,})
-        const onerror = (err:Error)=> {
-            this.slave = undefined;
-            console.error(err);
+        const client: Sentinel = (this as any).connectToSentinel(endpoint);
+        const result = await client.sentinel("slaves", this.options.name!);
+        const availableSlaves = result.map<AddressFromResponse>(
+            packObject as (value: any) => AddressFromResponse
+        )
+            .filter(
+                (slave) =>
+                    slave.flags && !slave.flags.match(/(disconnected|s_down|o_down)/)
+            );
+
+        const slaves = [];
+
+        for (const availableSlave of availableSlaves) {
+            const { sentinels, sentinelCommandTimeout, sentinelPassword, sentinelMaxConnections, sentinelReconnectStrategy, sentinelRetryStrategy, sentinelTLS, sentinelUsername, updateSentinels, enableTLSForSentinelMode, Connector, ...options } = (this.options as Redis.RedisOptions & Redis.SentinelConnectionOptions)
+            const slave = new Redis.default({ ...options, ...endpoint, })
+            slaves.push(slave);
         }
-        slave.once("error", onerror);
 
-
-        return this.slave = slave;
+        return slaves;
 
     }
+
+
 }
 
 export class RedisGraph extends Redis.default implements Redis.RedisCommander {
-    private slave?: RedisGraph;
-    constructor(private graphName: string,{role = 'master', ...options}: Redis.RedisOptions){
-        super({...options, role, Connector})
-        
+    private pool: Array<Redis.Redis> = []
+
+    constructor(private graphName: string, { role = 'master', ...options }: Redis.RedisOptions) {
+        super({ ...options, failoverDetector: !process.env["IOREDIS_MASTER_ONLY"], role, Connector });
+        if (!process.env["IOREDIS_MASTER_ONLY"]) {
+            this.once("connect", async () => {
+                const slaves = await ((this as any).connector as Connector).getSlaves();
+                this.pool.push(...slaves);
+            });
+        }
     }
 
-    async getSlave(){
-        const connector = (this as any).connector;
-       return connector.getSlave();
+
+    async getConnection(readOnly: boolean = true, cb: (redis: Redis.default) => Promise<any>) {
+        if (readOnly || process.env["IOREDIS_MASTER_ONLY"]) {
+            return cb(this);
+        }
+        const node = this.pool.shift();
+
+        if (!node) {
+            return cb(this);
+        }
+        this.pool.push(node);
+        return cb(node);
+
     }
 
     async query<T = unknown>(command: string, params: any, options: {
@@ -105,16 +136,16 @@ export class RedisGraph extends Redis.default implements Redis.RedisCommander {
 
         const { graphName = this.graphName, readOnly, timeout } = options;
         const graph = new Graph({ readOnly, graphName, timeout, });
-        
-        let node:any = readOnly ? await this.getSlave() ?? this: this;
-        const buf = await node.sendCommand(graph.query<T>(command, params));
+
+        const buf = await this.getConnection(readOnly, (node) => node.sendCommand(graph.query<T>(command, params)) as any)
         const response = new GraphResponse(graph, this, graph.options);
         return response.parse(buf as any as RedisGraphResponse) as any;
-    }
-}   
 
-Redis.Pipeline.prototype.query = function <T>(this: Redis.Pipeline & { redis: {graphName: string}, options: any }, query: string, params: Record<string, unknown>, options: { readOnly: boolean, graphName?: string } = { readOnly: true }) {
-    const { graphName =this.redis.graphName, readOnly } = options;
+    }
+}
+
+Redis.Pipeline.prototype.query = function <T>(this: Redis.Pipeline & { redis: { graphName: string }, options: any }, query: string, params: Record<string, unknown>, options: { readOnly: boolean, graphName?: string } = { readOnly: true }) {
+    const { graphName = this.redis.graphName, readOnly } = options;
     const graph = new Graph({ readOnly, graphName });
     this.sendCommand(graph.query(query, params));
     return this as Redis.Pipeline;
